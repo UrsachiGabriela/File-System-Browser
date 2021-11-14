516763534089c5ea2d41bc044d74f312bd0a86f5
ArinaAzamfirei <arina.azamfirei@student.tuiasi.ro>
2021-11-14 21:31:06 +0200

    Update

--- Client_CoAP/src/CoAP/constants.py
+++ Client_CoAP/src/CoAP/constants.py
@@ -1,1 +1,19 @@
 from enum import Enum
+
+DEFAULT_VERSION = 1
+ACK_TIMEOUT = 2
+ACK_RANDOM_FACTOR  = 1.5
+MAX_RETRANSMIT = 4
+
+class Type(Enum):
+    CON_MSG = 0
+    NON_MSG = 1
+    ACK = 2
+    RST = 3
+
+class Method(Enum):
+    EMPTY = 0.00
+    GET = 0.01
+    POST = 0.02
+    SEARCH = 0.08
+
